567d2543ffa9b2004b240de1e25e0b368dc61e79
Unknown <n.weaire@gmail.com>
2018-10-22 23:20:11 +1100

    Collision framework
    Added
     - Frame work for horizontal collisions

--- Assets/Scripts/TPC_Movement.cs
+++ Assets/Scripts/TPC_Movement.cs
@@ -5,78 +5,101 @@ using UnityEngine;
 public class TPC_Movement : MonoBehaviour
 {
 
-    public float m_fMovementSpeed = 5.0f;
-    private float m_fVerticalInput = 0;
-    private float m_fHorizontalInput = 0;
-    public LayerMask m_LayerMask;
-    public bool m_bIsGrounded = false;
-    public float m_fJumpForce = 20.0f;
-    public float m_fGravity = 5.0f;
-    public float m_fVerticalVelocity = 0;
+    public float m_fMovementSpeed = 5.0f; // Base movement speed
+    private float m_fMovementInput; // Vertical input
+    private float m_fCameraInput = 0; // Horizontal input
+    public LayerMask m_LayerMask; // Layer mask for collision checks
+    private CapsuleCollider m_cCapsuleCollider; // Reference to capsule collider
+    public bool m_bIsGrounded = false; // Checks if player is grounded
+    public float m_fJumpForce = 20.0f; // Jump velocity
+    public float m_fGravity = 5.0f; // Gravity velocity
+    public float m_fVerticalVelocity = 0; // Current vertical velocity
+    public float m_fCameraSpeed = 2f; // Speed of camera rotation
+    public int m_nDirY; // Direction of vertical movement
+    public int m_nDirZ; // Direction of horizontal movement
+    public int m_nHorizontalRayCount = 5;
+    public int m_nVerticalRayCount = 10;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        m_cCapsuleCollider = GetComponent<CapsuleCollider>();
+
+    }
+
     void Update()
     {
-        CheckIfGrounded();
-        MovementInput();
-        CameraRotation();
-        Gravity();
-        if (Input.GetButtonDown("Jump"))
+        CheckCollisions(); // Checks collisions based on velocity and direction
+        MovementInput(); // Gets current movement input and direction
+        Gravity(); // Applies gravity if needed
+        if (Input.GetButtonDown("Jump")) // Checks if Jump button pressed
         {
-            Jump();
+            Jump(); // Calls jump function based on grounded value
         }
-        Move();
+        Move(); // Applies velocity to player
     }
 
     public void MovementInput()
     {
-        m_fVerticalInput = Input.GetAxisRaw("Vertical");
+        m_fMovementInput = Input.GetAxisRaw("Vertical"); // Gets all vertical axis input
+        m_fCameraInput = Input.GetAxisRaw("Horizontal"); // Horizontal movement    
+        if (m_fVerticalVelocity > 0) // If vertical velocity is greater then 0
+        {
+            m_nDirY = 1; // Direction is 1 or forward/positive
+        }
+        else
+        {
+            m_nDirY = -1; // Direction is negative
+        }
+
     }
 
-    public void CheckIfGrounded()
+    public void CheckCollisions()
     {
-        Debug.DrawRay(this.transform.position, Vector3.down, Color.cyan);
-        if (Physics.Raycast(this.transform.position + new Vector3(0, 0.1f, 0), Vector3.down, 0.1f, m_LayerMask))
+        if (Physics.Raycast(this.transform.position, Vector3.up * m_nDirY, m_cCapsuleCollider.height * 0.5f, m_LayerMask) // Casts 5 rays around player for vertical movement based on direction
+            || Physics.Raycast(this.transform.position + new Vector3(m_cCapsuleCollider.radius, 0, m_cCapsuleCollider.radius), Vector3.up * m_nDirY, m_cCapsuleCollider.height * 0.5f, m_LayerMask)
+            || Physics.Raycast(this.transform.position + new Vector3(-m_cCapsuleCollider.radius, 0, m_cCapsuleCollider.radius), Vector3.up * m_nDirY, m_cCapsuleCollider.height * 0.5f, m_LayerMask)
+            || Physics.Raycast(this.transform.position + new Vector3(m_cCapsuleCollider.radius, 0, -m_cCapsuleCollider.radius), Vector3.up * m_nDirY, m_cCapsuleCollider.height * 0.5f, m_LayerMask)
+            || Physics.Raycast(this.transform.position + new Vector3(-m_cCapsuleCollider.radius, 0, -m_cCapsuleCollider.radius), Vector3.up * m_nDirY, m_cCapsuleCollider.height * 0.5f, m_LayerMask)
+            )
         {
-            m_bIsGrounded = true;
+            m_bIsGrounded = true; // If rays hit something is grounded
         }
         else
         {
             m_bIsGrounded = false;
         }
-    }
-
-    public void CameraRotation()
-    {
-        m_fHorizontalInput = Input.GetAxisRaw("Horizontal");
-        if (m_fVerticalInput >= 0)
+        for (int i = 0; i < m_nVerticalRayCount; i++)
         {
-            this.transform.Rotate(Vector3.up, m_fHorizontalInput);
+            Vector3 startPos = this.transform.position - new Vector3(0, (m_cCapsuleCollider.height * 0.45f) - (i * (m_cCapsuleCollider.height / m_nVerticalRayCount)), 0);
+            startPos -= transform.right * m_cCapsuleCollider.radius;
+            for (int j = 0; j <= m_nHorizontalRayCount; j++)
+            {
+                Debug.DrawRay(startPos + new Vector3(transform.right.x * j * ((m_cCapsuleCollider.radius * 2) / m_nHorizontalRayCount), 0, 0), transform.forward, Color.red);
+                if(Physics.Raycast(startPos + new Vector3(j * ((m_cCapsuleCollider.radius * 2) / m_nHorizontalRayCount), 0, 0), transform.forward, m_cCapsuleCollider.radius,m_LayerMask))
+                {
+                    //Debug.Log("hit");
+                }
+            }
         }
-        else
-        {
-            this.transform.Rotate(Vector3.up, -m_fHorizontalInput);
-        }
-
     }
 
     public void Gravity()
     {
-        if (m_bIsGrounded)
+        if (m_bIsGrounded) // If grounded
         {
-            m_fVerticalVelocity = 0;
+            m_fVerticalVelocity = 0; // Applies no gravity to vertical velocity
         }
         else
         {
-            m_fVerticalVelocity -= m_fGravity * Time.deltaTime;
+            m_fVerticalVelocity -= m_fGravity * Time.deltaTime; // If not grounded applies constant downward velocity
         }
     }
 
     public void Jump()
     {
-        if (m_bIsGrounded)
+        if (m_bIsGrounded) // If grounded
         {
-            m_fVerticalVelocity = m_fJumpForce;
+            m_fVerticalVelocity = m_fJumpForce; // Apply jump force to current vertical velocity
         }
         else
         {
@@ -86,6 +109,7 @@ public class TPC_Movement : MonoBehaviour
 
     public void Move()
     {
-        transform.Translate(new Vector3(0, m_fVerticalVelocity * Time.deltaTime, m_fVerticalInput * m_fMovementSpeed * Time.deltaTime));
+        transform.Translate(new Vector3( 0, m_fVerticalVelocity * Time.deltaTime, m_fMovementInput * m_fMovementSpeed * Time.deltaTime)); // Applies velocity based on input, move speed and vertical velocity
+        transform.Rotate(Vector3.up, m_fCameraInput * m_fCameraSpeed * Time.deltaTime); // Updates rotation based on horizontal input
     }
 }
